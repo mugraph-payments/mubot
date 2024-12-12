d16dabd65d38ffabeff76c52147967aa852d0a55
Lucas Barros <lucasnbarros@gmail.com>
2024-12-12 17:04:47 -0300

    feat: add litellm

--- mubot/ollama/__init__.py
+++ mubot/ollama/__init__.py
@@ -1,12 +1,18 @@
 """
 MuBot Ollama main module
 """
-
+from litellm import completion
 
 def main() -> None:
     """Main entry point for the application"""
 
-    print("Hello World!")
+    # print("Hello World!")
+    response = completion(
+        model="ollama/llama3.2:1b",
+        messages=[{ "content": "respond in 20 words. who are you?","role": "user"}],
+        api_base="http://localhost:11434"
+    )
+    print(response)
 
 
 if __name__ == "__main__":
